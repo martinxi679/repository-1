854205881cf4d2f7c2b64a5a0630c3ad707a4951
Raffi Yang <raffi.yang@gmail.com>
2017-02-01 10:31:29 -0500

    Added my name and GTid to AntLabMain.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("vsong3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Raffi Yang");
+                  toPrint += p5.toString("ryang71");
 		  
         System.out.println(toPrint);
         
